80c44363c6ad7decb905a8e2b7d4f4ef02eca83b
PrathyushaAlaghari <justlikepratuasha@gmail.com>
2024-04-19 16:33:48 +0530

    Section 2 API Skeleton

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[Controller]")] // /api/user [controller] means the name of the controller
+public class UserController : ControllerBase
+{
+    private readonly DataContext context;
+
+    public UserController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet] // one end point /api/user
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await this.context.Users.ToListAsync();
+        return users; 
+    }
+
+    [HttpGet("{id}")] // /api/user/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await this.context.Users.FindAsync(id);
+        return user;
+    }
+}
